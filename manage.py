bc57f34b4b5b554d6ef7a134aac99d7b059104c9
VishnuWorld <52811937+Vishnuworld@users.noreply.github.com>
2021-04-22 20:56:11 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -14,17 +14,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-def main_funcion():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Book.settings')
-    try:
-        print("ABC")
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Book.settings')
@@ -32,11 +21,6 @@ def main():
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
