175c04097fe2394b4869a1585836f9234aa1b770
Chetan Mahajan <37900974+c-mahajan@users.noreply.github.com>
2021-04-23 22:35:37 +0530

    "Added DreamReal Model."

--- bookinfo/models.py
+++ bookinfo/models.py
@@ -19,6 +19,16 @@ class Book(models.Model):
 
     class Meta:
         db_table = "book"
+        
+class Dreamreal(models.Model):
+
+   website = models.CharField(max_length = 50)
+   mail = models.CharField(max_length = 50)
+   name = models.CharField(max_length = 50)
+   phonenumber = models.IntegerField()
+
+   class Meta:
+      db_table = "dreamreal"
 
 
 
